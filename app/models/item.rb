8af435b8c18267f396308c40ea76363206a9232c
fuji114 <fujinuma.1414@gmail.com>
2021-08-12 18:28:24 +0900

    ActiveHashを導入する

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
+  with_options presence: true do
+    validates :product_name
+    validates :product_description
+    validates :category_id
+    validates :status_id
+    validates :invoice_price_id
+    validates :invoice_territory_id
+    validates :days_to_delivery_id
+    validates :product_price
+  end
 end
