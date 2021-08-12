6046f6816052a9c20c883f4fccfa62015d81eaf6
fuji114 <fujinuma.1414@gmail.com>
2021-08-12 16:29:40 +0900

    ルーティングとコントローラーにnewとcreateを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,13 @@
 class ItemsController < ApplicationController
+
   def index
+    @items = Item.all
   end
+
+  def new
+  end
+
+  def create
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,3 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :comments
 end
