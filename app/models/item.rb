eded1379558ffebca7c6b8a5389c039d85b1e868
fuji114 <fujinuma.1414@gmail.com>
2021-08-16 12:28:36 +0900

    修正依頼の解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ApplicationRecord
     validates :image
     validates :product_name
     validates :product_description, length: { maximum: 1000 }
-    validates :product_price, inclusion: { in: 300..9999999 }
+    validates :product_price, inclusion: { in: 300..9999999 }, numericality: {only_integer: true}
   end
 
   validates :category_id,:status_id,:invoice_price_id,:invoice_territory_id,:days_to_delivery_id, numericality: { other_than: 1 , message: "can't be blank"} 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -27,45 +27,93 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Product description can't be blank")
       end
-      it '商品の詳細のカテゴリーが"--"以外' do
+
+      it '商品の詳細のカテゴリーが空欄' do
         @item.category_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it '商品の詳細の商品の状態が"--"以外' do
+      it '商品の詳細のカテゴリーが"--"を選択した場合登録できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it '商品の詳細の商品の状態が空欄' do
         @item.status_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
-      it '配送についての配送料の負担が"--"以外' do
+      it '商品の詳細の商品の状態が"--"を選択した場合登録できない' do
+        @item.status_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Status can't be blank")
+      end
+      it '配送についての配送料の負担が空欄' do
         @item.invoice_price_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Invoice price can't be blank")
       end
-      it '配送についての発送元の地域が"--"以外' do
+      it '配送についての配送料の負担が"--"を選択した場合登録できない' do
+        @item.invoice_price_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Invoice price can't be blank")
+      end
+      it '配送についての発送元の地域が空欄' do
         @item.invoice_territory_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Invoice territory can't be blank")
       end
-      it '配送についての発送までの日数が"--"以外' do
+      it '配送についての発送元の地域が"--"を選択した場合登録できない' do
+        @item.invoice_territory_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Invoice territory can't be blank")
+      end
+      it '配送についての発送までの日数が空欄' do
         @item.days_to_delivery_id = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Days to delivery can't be blank")
       end
-      it '商品の詳細の販売価格が必須' do
+      it '配送についての発送までの日数が"--"を選択した場合登録できない' do
+        @item.days_to_delivery_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to delivery can't be blank")
+      end
+
+      it '販売価格が必須' do
         @item.product_price = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Product price can't be blank")
       end
-      it '商品の詳細の販売価格が300円以下' do
+      it '販売価格が300円以下' do
         @item.product_price = '299'
         @item.valid?
         expect(@item.errors.full_messages).to include("Product price is not included in the list")
         end
-      it '商品の詳細の販売価格が9999999円以上' do
+      it '販売価格が9999999円以上' do
         @item.product_price = '1000000000'
         @item.valid?
         expect(@item.errors.full_messages).to include("Product price is not included in the list")
+      end
+
+      it 'ユーザー情報がない場合登録できないこと' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+      it '商品の販売価格が全角文字では登録できないこと' do
+        @item.product_price = '１００００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product price is not included in the list")
+      end
+      it '商品の販売価格が半角英数混合では登録できないこと' do
+        @item.product_price = '10000O'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product price is not a number")
+      end
+      it '商品の販売価格が半角英語のみでは登録できないこと' do
+        @item.product_price = 'IOOOO'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Product price is not a number")
       end
     end
   end
